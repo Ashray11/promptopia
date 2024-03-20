69e575673fc9e785b74987e1567dbcb522f6310d
ashray kothari <ashrayk11@gmail.com>
2024-03-19 18:41:47 -0700

    Correct-2 Missing Suspense boundary with useSearchParams

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect, Suspense } from "react";    
+import { useState, useEffect } from "react";    
 import { useRouter, useSearchParams } from "next/navigation";
 import Form from "@components/Form";
 
@@ -57,15 +57,13 @@ const UpdatePrompt = () => {
     }
 
     return (
-        <Suspense>
-            <Form
-                type="Update"
-                post={post}
-                setPost={setPost}
-                submitting={submitting}
-                handleSubmit={updatePrompt}
-                />
-        </Suspense>
+        <Form
+            type="Update"
+            post={post}
+            setPost={setPost}
+            submitting={submitting}
+            handleSubmit={updatePrompt}
+        />
     )
 }
 
